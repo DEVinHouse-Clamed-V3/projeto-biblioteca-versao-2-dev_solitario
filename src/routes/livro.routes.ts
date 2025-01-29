57c44eed7110ba5482386d930a726b4b5ada1658
Cleiton Ricardo Spagnol <101300606+cleitonricardo@users.noreply.github.com>
2025-01-29 20:53:29 -0300

    Update livro.routes.ts

--- src/routes/livro.routes.ts
+++ src/routes/livro.routes.ts
@@ -1,6 +1,24 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
+import { BookController } from '../controllers/book.controller';
+
 
 const livroRoutes = Router();
+// Criar um livro
+livroRoutes.post("/", BookController.createBook);
 
+// Buscar todos os livros
+livroRoutes.get("/", BookController.getAllBooks);
+
+// Buscar um livro específico por ID
+livroRoutes.get("/:id", BookController.getBookById);
+
+// Atualizar um livro
+livroRoutes.put("/:id", BookController.updateBook);
+
+// Deletar um livro
+livroRoutes.delete("/:id", BookController.deleteBook);
+
+// Buscar o ranking dos livros
+livroRoutes.get("/ranking", BookController.getBookRanking);
 
 export default livroRoutes;
