251472d42ac871eac63c7363dbc32c6ad9b181bb
DominiceLanzarini <dlanzarini@gmail.com>
2025-02-03 17:11:58 -0300

    Entidade Leitor criada

--- src/entities/Leitor.ts
+++ src/entities/Leitor.ts
@@ -1,5 +1,31 @@
-class Leitor {
-    
-}
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
-export default Leitor;
+@Entity("readers")
+export default class Leitor {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({name:"name"})
+  nome: string;
+
+  @Column()
+  email: string;
+
+  @Column({name: "phone_number"})
+  telefone: string;
+
+  @Column({name:"birthdate"})
+  dataNascimento: Date;
+
+  @Column({name:"address", type: 'text'})
+  endereco: string;
+
+  @Column({name:"active"})
+  ativo: boolean;
+
+  @Column({name:"created_at"})
+  criadoEm: Date;
+
+  @Column({name:"updated_at"})
+  atualizadoEm: Date;
+}
